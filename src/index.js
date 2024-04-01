54e17f000c17331a3a684a5591df2d9b7e554777
asseternity <nakupovasset@gmail.com>
2024-04-01 20:16:09 +0500

    Dropdown menu module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+import DropdownCreator from "./dropdown_creator";
+
+let dropdownCreator = new DropdownCreator();
+
+dropdownCreator.CreateDropdown("dropdown_button_1", "dropdown_content");
